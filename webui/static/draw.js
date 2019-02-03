b676771e98400584a56cf1ad1245bd727526c196
David Čepelík <david.cepelik@showmax.com>
2019-02-03 10:09:19 +0000

    rewritten webui javascript pieces

--- webui/static/draw.js
+++ webui/static/draw.js
@@ -1,240 +1,178 @@
-function canvas() {
+function getCanvas() {
 	return document.getElementById("myCanvas");
 }
 
-var zoomf = 1;
-var margin = zoomf * 2;
-var c = canvas();
-var dragdrop = false;
-var ctx = c.getContext("2d");
-var width, height;
-
-var active;
-c.addEventListener('mousedown', function(e) {
-	var rect = e.target.getBoundingClientRect();
-	var x = e.clientX - rect.left; //x position within the element.
-	var y = e.clientY - rect.top;  //y position within the element.
-	active = findHoverPgon(x, y);
-	redraw();
-}, true);
-c.addEventListener('mouseup', function(e) {
-	startX = startY = undefined;
-	active = undefined;
-	redraw();
-}, true);
-
-function findHoverPgon(x, y) {
-	for (let i = 0; i < pgons.length; i++)
-		if (pgons[i].isInside(x, y))
-			return pgons[i];
+async function get(url) {
+	console.log(`GET ${url}`)
+	resp = await fetch(url)
+	if (!resp.ok)
+		throw `Failed to GET ${url}: ${resp.status}: ${resp.statusText}`
+	return resp.json()
 }
 
-var fsize;
+function drawPoly(ctx, pts, fill) {
+	ctx.beginPath()
+	ctx.moveTo(...pts[0])
+	for (pt of pts)
+		ctx.lineTo(...pt)
+	ctx.closePath()
+	ctx.strokeStyle = '#000000'
+	if (fill)
+		ctx.fill()
+	else
+		ctx.stroke()
+}
 
-class Polygon {
-	constructor(pts, fill=false) {
-		this.pts = pts;
-		this.fill = fill;
-		this.acitve = false;
+function drawEllipse(ctx, x, y, a, b) {
+	ctx.beginPath()
+	ctx.ellipse(x, y, a, b, 0, 0, 2 * Math.PI, false)
+	ctx.closePath()
+	ctx.stroke()
+}
+
+function drawSpline(ctx, pts) {
+	ctx.beginPath()
+	ctx.moveTo(...pts[0])
+	for (pt of pts)
+		ctx.lineTo(...pt)
+	ctx.stroke()
+}
+
+function drawText(ctx, x, y, w, text) {
+	ctx.save()
+	ctx.translate(x - w/2, y)
+	ctx.scale(1, -1)
+	ctx.fillText(text, 0, 0)
+	ctx.restore()
+}
+
+function drawBbox(ctx, pts) {
+	ctx.save()
+	let xmin, xmax, ymin, ymax
+	xmin = ymin = Number.POSITIVE_INFINITY
+	xmax = ymax = Number.NEGATIVE_INFINITY
+	for ([x, y] of pts) {
+		if (x > xmax)
+			xmax = x
+		if (x < xmin)
+			xmin = x
+		if (y > ymax)
+			ymax = y
+		if (y < ymin)
+			ymin = y
 	}
+	console.log(xmin, ymin, xmax, ymax)
+	ctx.strokeStyle = '#ff0000'
+	let m = 10
+	ctx.beginPath()
+	ctx.rect(xmin - m, ymin - m, xmax - xmin + 2 * m, ymax - ymin + 2 * m)
+	ctx.stroke()
+	ctx.strokeStyle = '#000000'
+	ctx.restore()
+}
 
-	draw(ctx) {
-		ctx.save();
-		ctx.beginPath();
-		ctx.moveTo(...this.pts[0]);
-		ctx.strokeStyle = this.active ? '#ff0000' : '#000000';
-		for (let j = 1; j < this.pts.length; j++) {
-			ctx.lineTo(...this.pts[j]);
+function run(ctx, prog) {
+	ctx.save()
+	let params = {}
+	for (cmd of prog) {
+		switch (cmd.op) {
+		case 'p':
+		case 'P':
+			drawPoly(ctx, cmd.points, (cmd.op == 'P'))
+			drawBbox(ctx, cmd.points)
+			break;
+		case 'e':
+			let [cx, cy, a, b] = cmd.rect
+			console.log(cx, cy)
+			drawBbox(ctx, [
+				[cx - a, cy - b],
+				[cx + a, cy + b]
+			])
+			drawEllipse(ctx, ...cmd.rect)
+			break;
+		case 'b':
+			drawSpline(ctx, cmd.points)
+			break;
+		case 'T':
+			drawText(ctx, ...cmd.pt, cmd.width, cmd.text)
+			break;
+		case 'F':
+			ctx.font = `${cmd.size}px ${cmd.face}`
+			break;
+		default:
+			/* ignore unknown commands */
 		}
-		ctx.closePath();
-		if (this.fill)
-			ctx.fill();
-		else
-			ctx.stroke()
-		ctx.restore();
 	}
-
-	crosses(ux0, uy0, ux1, uy1, vx0, vy0, vx1, vy1) {
-		let a = uy1 - uy0;
-		let b = ux0 - ux1;
-		let c = ux1 * uy0 - ux0 * uy1;
-		let d1 = a * vx0 + b * vy0 + c;
-		let d2 = a * vx1 + b * vy1 + c;
-		return (d1 > 0 && d2 < 0) || (d1 < 0 && d2 > 0);
-	}
-
-	isInside(x, y) {
-		let intersect = 0;
-		let xmin = Number.POSITIVE_INFINITY;
-		for (let i = 0; i < this.pts.length; i++) {
-			if (this.pts[i][0] < xmin)
-				xmin = this.pts[i][0];
-		}
-		let rx0 = xmin - 2, ry0 = y - 2, rx1 = x, ry1 = y;
-		for (let i = 0, j = 1; i < this.pts.length; i++, j++) {
-			if (j == this.pts.length)
-				j = 0;
-			if (this.crosses(rx0, ry0, rx1, ry1, ...this.pts[i], ...this.pts[j])
-				&& this.crosses(...this.pts[i], ...this.pts[j], rx0, ry0, rx1, ry1))
-				intersect++; // TODO collinearity!
-		}
-		return intersect % 2 == 1;
-	}
+	ctx.restore()
 }
 
-var pgons = [];
-
-class Ellipse {
-	constructor(x, y, a, b) {
-		this.x = x;
-		this.y = y;
-		this.a = a;
-		this.b = b;
+function draw(data) {
+	let zoomf = 1;
+	let [x, y, w, h] = data.bb.split(',').map(s => Math.floor(parseFloat(s)));
+	let c = getCanvas()
+	let margin = zoomf * 2;
+	c.width = Math.floor(zoomf * w + 2 * margin);
+	c.height = Math.floor(zoomf * h + 2 * margin);
+	let ctx = c.getContext('2d')
+	ctx.translate(margin, margin + h);
+	ctx.scale(zoomf, -zoomf);
+	for (obj of data['objects']) {
 	}
-
-	draw(ctx) {
-		ctx.beginPath();
-		ctx.ellipse(this.x, this.y, this.a, this.b, 0, 0, 2 * Math.PI, false);
-		ctx.closePath();
-		ctx.stroke();
-	}
-}
-
-class Text {
-	constructor(pt, text) {
-		this.pt = pt;
-		this.text = text;
-	}
-
-	draw(ctx) {
-		ctx.save();
-		ctx.translate(...this.pt);
-		ctx.fillText(this.text, 0, 0);
-		ctx.restore();
-	}
-}
-
-class BSpline {
-	constructor(pts) {
-		this.pts = pts;
-	}
-
-	draw(ctx) {
-		ctx.beginPath();
-		ctx.moveTo(...this.pts[0]);
-		for (let j = 1; j < this.pts.length; j++)
-			ctx.lineTo(...this.pts[j]);
-		ctx.stroke();
-	}
-}
-
-var elems = [];
-
-function drawit(objs, prop) {
-	for (let o = 0; o < objs.length; o++) {
-		let obj = objs[o];
-		let draw = obj[prop];
-		if (!draw)
-			return;
-		let pos = obj.pos;
-		let [x0, y0] = pos.split(',').map(s => parseFloat(s));
-		let pts;
-		for (let i = 0; i < draw.length; i++) {
-			let op = draw[i].op;
-			switch (op) {
-				case "p":
-				case "P":
-					pts = draw[i].points;
-					for (let k = 0; k < pts.length; k++)
-						pts[k][1] = height - pts[k][1];
-					poly = new Polygon(pts, op == "P");
-					elems.push(poly);
-					pgons.push(poly);
-					break;
-
-				case "e":
-					r = draw[i].rect;
-					r[1] = height - r[1];
-					ell = new Ellipse(...r);
-					elems.push(ell);
-					break;
-
-				case "b":
-					pts = draw[i].points;
-					for (let k = 0; k < pts.length; k++)
-						pts[k][1] = height - pts[k][1];
-					bsp = new BSpline(pts);
-					elems.push(bsp);
-					break;
-				case "F":
-					fsize = Math.floor(draw[i].size)
-					ctx.font = fsize + "px " + draw[i].face;
-					ctx.textAlign = "center";
-					break;
-				case "T":
-					draw[i].pt[1] = height - draw[i].pt[1];
-					txt = new Text(draw[i].pt, draw[i].text);
-					elems.push(txt);
-					break;
+	for (kind of ['objects', 'edges']) {
+		for (obj of data[kind]) {
+			for (s of ['_draw_', '_ldraw_']) {
+				if (s in obj) {
+					run(ctx, obj[s])
+				}
 			}
 		}
 	}
+			
 }
 
-function redraw() {
-	ctx.clearRect(0, 0, c.width, c.height);
-	ctx.moveTo(0, 0);
-	for (let i = 0; i < elems.length; i++) {
-		elems[i].active = (active == elems[i]);
-		elems[i].draw(ctx);
-	}
-}
-
-async function get(url) {
-	return fetch(url).then((r) => r.json())
-}
-
-async function all() {
+async function fetchAndDraw() {
 	var data = await get('graph.json')
-	console.log(data)
-
-	let bb = data.bb.split(',').map(s => Math.floor(parseFloat(s)));
-	width = bb[2], height = bb[3];
-	c.width = Math.floor(zoomf * width + 2 * margin);
-	c.height = Math.floor(zoomf * height + 2 * margin);
-	//ctx.translate(margin, margin);
-	ctx.scale(zoomf, zoomf);
-
-	drawit(data.objects, '_draw_');
-	drawit(data.objects, '_ldraw_');
-	drawit(data.edges, '_draw_');
-	drawit(data.edges, '_hdraw_');
-	drawit(data.edges, '_ldraw_');
-
-	redraw();
+	draw(data)
 }
 
-all()
+function main() {
+	var c = getCanvas();
+	c.addEventListener('mousedown', function(e) {
+		var rect = e.target.getBoundingClientRect();
+		var x = e.clientX - rect.left; //x position within the element.
+		var y = e.clientY - rect.top;  //y position within the element.
+		active = findHoverPgon(x, y);
+		//redraw();
+	}, true);
+	c.addEventListener('mouseup', function(e) {
+		startX = startY = undefined;
+		active = undefined;
+		//redraw();
+	}, true);
 
-var startX, startY;
-c.addEventListener('mousemove', function(e) {
-	var rect = e.target.getBoundingClientRect();
-	var x = e.clientX - rect.left;
-	var y = e.clientY - rect.top;
-	if (active) {
-		if (startX == undefined) {
+	var startX, startY;
+	c.addEventListener('mousemove', function(e) {
+		var rect = e.target.getBoundingClientRect();
+		var x = e.clientX - rect.left;
+		var y = e.clientY - rect.top;
+		if (active) {
+			if (startX == undefined) {
+				startX = x;
+				startY = y;
+			}
+			dx = x - startX;
+			dy = y - startY;
+			for (let i = 0; i < active.pts.length; i++) {
+				active.pts[i][0] += dx;
+				active.pts[i][1] += dy;
+			}
 			startX = x;
 			startY = y;
 		}
-		dx = x - startX;
-		dy = y - startY;
-		for (let i = 0; i < active.pts.length; i++) {
-			active.pts[i][0] += dx;
-			active.pts[i][1] += dy;
-		}
-		startX = x;
-		startY = y;
-	}
-	redraw();
-	//console.log("Active poly:", active);
-}, true);
+		//redraw();
+		//console.log("Active poly:", active);
+	}, true);
+	fetchAndDraw()
+}
+
+main()
